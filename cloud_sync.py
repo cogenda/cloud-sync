28270f8196a0e1b7d4e52a9c7ed68737cc8dacd8
tim.tang <tang.jilong@139.com>
2014-06-16 13:52:57 +0800

    [cloud-sync] - Add retry strategy for sync failed files.

--- cloud_sync.py
+++ cloud_sync.py
@@ -106,8 +106,12 @@ class CloudSync(threading.Thread):
         for server in TRANSPORTERS:
             self.transport_queue[server] = AdvancedQueue()
 
+        self.failed_files = PersistentList("failed_files_list", PERSISTENT_DATA_DB)
+        num_failed_files = len(self.failed_files)
+        self.logger.warning("Setup: initialized 'failed_files' persistent list, contains %d items." % (num_failed_files))
         self.discover_queue  = Queue.Queue()
         self.db_queue = Queue.Queue()
+        self.retry_queue = Queue.Queue()
 
         # Create connection to synced files DB.
         self.dbcon = sqlite3.connect(SYNCED_FILES_DB)
@@ -145,9 +149,8 @@ class CloudSync(threading.Thread):
                 self.__process_discover_queue()
                 self.__process_transport_queues()
                 self.__process_db_queue()
-                #self.__process_files_to_delete()
-                #self.__process_retry_queue()
-                #self.__allow_retry()
+                self.__process_retry_queue()
+                self.__allow_retry()
 
                 # Processing the queues 5 times per second is more than sufficient
                 # because files are modified, processed and transported much
@@ -266,6 +269,52 @@ class CloudSync(threading.Thread):
                 processed += 1
 
 
+    def __process_retry_queue(self):
+        processed = 0
+
+        while processed < QUEUE_PROCESS_BATCH_SIZE and self.retry_queue.qsize() > 0:
+            # Retry queue -> failed files list.
+            # And remove from files in pipeline.
+            self.lock.acquire()
+            (input_file, event) = self.retry_queue.get()
+
+            if (input_file, event) not in self.failed_files:
+                self.failed_files.append((input_file, event))
+                already_in_failed_files = False
+            else:
+                already_in_failed_files = True
+            self.lock.release()
+
+            # Log.
+            if not already_in_failed_files:
+                self.logger.warning("Retry queue -> 'failed_files' persistent list: '%s'. Retrying later." % (input_file))
+            else:
+                self.logger.warning("Retry queue -> 'failed_files' persistent list: '%s'. File already being retried later." % (input_file))
+            processed += 1
+
+
+    def __allow_retry(self):
+        num_failed_files = len(self.failed_files)
+        should_retry = self.last_retry + RETRY_INTERVAL < time.time()
+        
+        if num_failed_files > 0 and should_retry:
+            failed_items = []
+
+            processed = 0
+            while processed < QUEUE_PROCESS_BATCH_SIZE and processed < len(self.failed_files):
+                item = self.failed_files[processed]
+                failed_items.append(item)
+                for server in TRANSPORTERS:
+                    self.transport_queue[server].put(item[0], item[1], server, item[0])
+                processed += 1
+            
+            for item in failed_items:
+                self.failed_files.remove(item)
+
+            self.last_retry = time.time()
+            self.logger.warning("Moved %d items from the 'failed_files' persistent list into the 'pipeline' persistent queue." % (processed))
+
+
     def __process_db_queue(self):
         processed = 0 
         while processed < QUEUE_PROCESS_BATCH_SIZE and self.db_queue.qsize() > 0:
@@ -294,8 +343,6 @@ class CloudSync(threading.Thread):
                     # the input_file that has been transported.
                     self.dbcur.execute("UPDATE synced_files SET transported_file_basename=?, url=? WHERE input_file=? AND server=?", (transported_file_basename, url, input_file, server))
                     self.dbcon.commit()
-
-
 
 
     def __get_transporter(self, server):
@@ -448,8 +495,7 @@ class CloudSync(threading.Thread):
                     (curried): input_file='%s'
                     (curried): event=%d""" % (input_file, event)
 
-        #TODO:           
-        #self.retry_queue.put((input_file, event))
+        self.retry_queue.put((input_file, event))
 
 
     def __calculate_transporter_dst(self, src, relative_paths=[]):
---
+++ persistent/persistent_list.py
@@ -0,0 +1,74 @@
+"""
+An infinite persistent list that uses sqlite for storage and a list for a complete in-memory cache
+"""
+
+
+import sqlite3
+import cPickle
+
+
+# Define exceptions.
+class PersistentListError(Exception): pass
+
+class PersistentList(object):
+    """a persistent queue with sqlite back-end designed for finite lists"""
+
+    def __init__(self, table, dbfile="persistent_list.db"):
+        # Initialize the database.
+        self.dbcon = None
+        self.dbcur = None
+        self.table = table
+        self.__prepare_db(dbfile)
+
+        # Initialize the memory list: load its contents from the database.
+        self.memory_list = {}
+        self.dbcur.execute("SELECT id, item FROM %s ORDER BY id ASC" % (self.table))
+        resultList = self.dbcur.fetchall()
+        for id, item in resultList:
+            self.memory_list[item] = id
+
+
+    def __prepare_db(self, dbfile):
+        sqlite3.register_converter("pickle", cPickle.loads)
+        self.dbcon = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        self.dbcon.text_factory = unicode # This is the default, but we set it explicitly, just to be sure.
+        self.dbcur = self.dbcon.cursor()
+        self.dbcur.execute("CREATE TABLE IF NOT EXISTS %s(id INTEGER PRIMARY KEY AUTOINCREMENT, item pickle)" % (self.table))
+        self.dbcon.commit()
+
+
+    def __contains__(self, item):
+        return item in self.memory_list.keys()
+
+
+    def __iter__(self):
+        return self.memory_list.__iter__()
+
+
+    def __len__(self):
+        return len(self.memory_list)
+
+
+    def __getitem__(self, index):
+        keys = self.memory_list.keys()
+        return keys[index]
+
+
+    def append(self, item):
+        # Insert the item into the database.
+        pickled_item = cPickle.dumps(item, cPickle.HIGHEST_PROTOCOL)
+        self.dbcur.execute("INSERT INTO %s (item) VALUES(?)" % (self.table), (sqlite3.Binary(pickled_item), ))
+        self.dbcon.commit()
+        id = self.dbcur.lastrowid
+        # Insert the item into the in-memory list.
+        self.memory_list[item] = id
+
+
+    def remove(self, item):
+        # Delete from the database.
+        if self.memory_list.has_key(item):
+            id = self.memory_list[item]
+            self.dbcur.execute("DELETE FROM %s WHERE id = ?" % (self.table), (id, ))
+            self.dbcon.commit()        
+            # Delete from the in-memory list.
+            del self.memory_list[item]
