930746f77d2864ed3768bf8f288f4b14abf63d11
tim.tang <tang.jilong@139.com>
2014-06-27 08:18:24 +0800

    [cloud-sync] - Add destroy resource api.

--- helper/sync_helper.py
+++ helper/sync_helper.py
@@ -14,25 +14,42 @@ class SyncHelper(object):
     WS_HOST='http://localhost:8088'
     API_AUTHENTICATION='/security/authenticate-ws'
     API_MODIFY_RESOURCE='/api/modify-resource'
+    API_DESTROY_RESOURCE='/api/destroy-resource'
     BASIC_AUTH=json.dumps({'username': 'cogenda', 'password':'cogenda'})
+
+
+    @staticmethod
+    def authenticate():
+        headers = {'content-type': 'application/json'}
+        resp = requests.post('%s%s' %(SyncHelper.WS_HOST, SyncHelper.API_AUTHENTICATION), data=SyncHelper.BASIC_AUTH, headers=headers)
+        if resp.status_code != 200:
+            return None
+        result = json.loads(resp.json())
+        if result['auth_success']:
+            return result['auth_token']
+        return None
+
 
     @staticmethod
     def sync_resource(auth_token, filename, url, status, server):
         payload = json.dumps({'filename': filename, 'url': url, 'status': status, 'server': server, 'type': '0'})
         headers = {'content-type': 'application/json', 'Authorization': auth_token}
         response = requests.post('%s%s' %(SyncHelper.WS_HOST, SyncHelper.API_MODIFY_RESOURCE), data=payload, headers=headers)
-        print response.json()
-        return json.loads(response.json())['success']
-        
+        if response.status_code != 200:
+            return None
+        return json.loads(response.json())
 
-    @staticmethod
-    def authenticate():
-        headers = {'content-type': 'application/json'}
-        resp = requests.post('%s%s' %(SyncHelper.WS_HOST, SyncHelper.API_AUTHENTICATION), data=SyncHelper.BASIC_AUTH, headers=headers)
-        result = json.loads(resp.json())
-        if result['auth_success']:
-            return result['auth_token']
-        return None
+
+    @staticmethod    
+    def destroy_resource(auth_token, filename, server):
+        payload = json.dumps({'filename': filename, 'server': server})
+        headers = {'content-type': 'application/json', 'Authorization': auth_token}
+        response = requests.post('%s%s' %(SyncHelper.WS_HOST, SyncHelper.API_DESTROY_RESOURCE), data=payload, headers=headers)
+        if response.status_code != 200:
+            return None
+        return json.loads(response.json())
+
+
 
 
 if __name__ == '__main__':
@@ -43,4 +60,5 @@ if __name__ == '__main__':
     print '-----------------------'
     print auth_token
     print '-----------------------'
-    SyncHelper.sync_resource(auth_token, 'xxxxxx', 'http://test.com/xx.png', '1', 'oss')
+    print SyncHelper.sync_resource(auth_token, 'xxxxxx', 'http://test.com/xx.png', '1', 'oss')
+    print SyncHelper.destroy_resource(auth_token, 'xxxxxx', 'oss')
