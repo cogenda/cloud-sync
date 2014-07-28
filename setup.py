e37deb13a7c814454d64716792e5082873935f11
tim.tang <tang.jilong@139.com>
2014-07-28 08:38:28 +0800

    [cloud-sync] - Update setup.

--- setup.py
+++ setup.py
@@ -1,3 +1,27 @@
 # -*- coding:utf-8 -*-
 
-# TODO:
+from setuptools import setup, find_packages
+
+install_requires = [
+    'django',
+    'django-storages'
+    'boto',
+    'requests'
+]
+
+setup(
+    name='cloud-sync',
+    version='0.0.1',
+    author='cogenda-dev-team',
+    author_email='support@cogenda.com',
+    url='http://cogenda.com',
+    packages=find_packages(exclude=['tests', 'test_*']), 
+    install_requires=install_requires,
+    zip_safe=False,
+    include_package_data=True,
+    **kwargs
+)
+
+
+
+
