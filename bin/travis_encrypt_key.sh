3a61ed6639f4e7c7837a46e4669e804700dce65e
tim.tang <tang.jilong@139.com>
2014-07-29 11:01:08 +0800

    [cloud-sync] - Tunning CI.

--- bin/travis_encrypt_key.sh
+++ bin/travis_encrypt_key.sh
@@ -6,5 +6,5 @@ then
 fi
 # To generate secure SSH deploy key for a github repo to be used from Travis
 base64 --wrap=0 ~/.ssh/id_rsa_deploy > ~/.ssh/id_rsa_deploy_base64
-ENCRYPTION_FILTER="echo \$(echo \"- secure: \")\$(travis encrypt \"\$FILE='\`cat $FILE\`'\" -r cogenda/cogenda-web)"
+ENCRYPTION_FILTER="echo \$(echo \"- secure: \")\$(travis encrypt \"\$FILE='\`cat $FILE\`'\" -r cogenda/cloud-sync)"
 split --bytes=100 --numeric-suffixes --suffix-length=2 --filter="$ENCRYPTION_FILTER" ~/.ssh/id_rsa_deploy_base64 id_rsa_
--- bin/travis_encrypt_key_mac.sh
+++ bin/travis_encrypt_key_mac.sh
@@ -6,7 +6,7 @@ fi
 
 # To generate secure SSH deploy key for a github repo to be used from Travis
 base64 --break=0 ~/.ssh/id_rsa_deploy > ~/.ssh/id_rsa_deploy_base64
-ENCRYPTION_FILTER="echo \$(echo \"- secure: \")\$(travis encrypt \"\$FILE='\`cat $FILE\`'\" -r floydpink/harimenon.com)"
+ENCRYPTION_FILTER="echo \$(echo \"- secure: \")\$(travis encrypt \"\$FILE='\`cat $FILE\`'\" -r cogenda/cloud-sync)"
 # If you don't have homebrew please install it from http://brew.sh/
 if [[ ! $(which brew) ]]
 then
