fdca211fa974f665986c7489aa893c0e1d9d1d5a
Tim.Tang <tang.jilong@139.com>
2014-07-29 12:55:23 +0800

    [cloud-sync] - Update fabfile.

--- fabfile.py
+++ fabfile.py
@@ -71,7 +71,7 @@ def restart_app():
         #run("cat /tmp/cloud_sync.pid | xargs kill -9")
         run("ps -ef | grep 'cloud_sync' | grep -v 'grep' | awk '{print $2}' | xargs kill -9")
         run("python -m cloud_sync_app.cloud_sync pub &")
-        run('ps -ef | grep 'cloud_sync'')
+        run("ps -ef | grep 'cloud_sync'")
     print(red("Restart Cloud Sync Service Succeed!"))
 
 def clean():
